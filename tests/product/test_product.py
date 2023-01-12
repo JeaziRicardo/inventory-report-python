3fe41dabf701e9dcf8ab719f8c6e1ee9944e1534
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2023-01-11 23:01:50 -0300

    feat: cria o teste da classe Product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "Chocolate",
+        "Garoto",
+        "11/01/2023",
+        "11/01/2024",
+        99999,
+        "abaixo de 8°C",
+    )
+    assert product.id == 1
+    assert product.nome_do_produto == "Chocolate"
+    assert product.nome_da_empresa == "Garoto"
+    assert product.data_de_fabricacao == "11/01/2023"
+    assert product.data_de_validade == "11/01/2024"
+    assert product.numero_de_serie == 99999
+    assert product.instrucoes_de_armazenamento == "abaixo de 8°C"
