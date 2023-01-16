a3420b75f6cd53ae97ab7fa0059d04e5226080bf
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2023-01-16 19:29:39 -0300

    feat(02): Gerar a versão simplificada do relatório

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,35 @@
+from datetime import date
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(products):
+        earliest_date = None
+        closest_expiration_date = None
+        companies = []
+
+        for product in products:
+            if (
+                earliest_date is None
+                or product["data_de_fabricacao"] < earliest_date
+            ):
+                earliest_date = product["data_de_fabricacao"]
+
+            if (
+                closest_expiration_date is None
+                or product["data_de_validade"] > str(date.today())
+                and product["data_de_validade"] < closest_expiration_date
+            ):
+                closest_expiration_date = product["data_de_validade"]
+
+            companies.append(product['nome_da_empresa'])
+
+        companies_counter = Counter(companies)
+        most_products_company = companies_counter.most_common()[0][0]
+
+        return (
+            f"Data de fabricação mais antiga: {earliest_date}\n"
+            f"Data de validade mais próxima: {closest_expiration_date}\n"
+            f"Empresa com mais produtos: {most_products_company}"
+        )
