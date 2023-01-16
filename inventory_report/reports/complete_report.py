815b23be5daaa4d2db68442c91507f0e60924bcc
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2023-01-16 20:19:11 -0300

    feat(03): Gerar a versão completa do relatório

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,23 @@
-class CompleteReport:
-    pass
+from .simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(products):
+        simple_report = SimpleReport.generate(products)
+        companies = []
+
+        for product in products:
+            companies.append(product["nome_da_empresa"])
+
+        companies_counter = Counter(companies)
+        products_by_company = ""
+        for company, qty in companies_counter.most_common():
+            products_by_company += f"- {company}: {qty}\n"
+
+        return (
+            f"{simple_report}\n"
+            "Produtos estocados por empresa:\n"
+            f"{products_by_company}"
+        )
